f9628a248a8e30edf1657c15c1566d9350fe31db
xfrolk03 <60733738+xfrolk03@users.noreply.github.com>
2020-03-27 21:29:28 +0100

    Update traffic_light.vhd

--- Labs/08-traffic_lights/traffic_light.vhd
+++ Labs/08-traffic_lights/traffic_light.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity traffic is
-	port (clk		:	in STD_LOGIC;	
-          reset		:	in STD_LOGIC;	
-     	  light 	:	out STD_LOGIC_VECTOR(5 downto 0) 
+	port (clk	:	in STD_LOGIC;	
+              reset	:	in STD_LOGIC;	
+     	      light 	:	out STD_LOGIC_VECTOR(5 downto 0) 
 	);
 end traffic;
 architecture traffic of traffic is	    
@@ -19,9 +19,9 @@ architecture traffic of traffic is
 begin
     process (clk, reset)
     begin
-		if reset 	=	'1' then
-        	state  <=	RG;
-            count  <=	X"0";
+		if reset = '1' then
+        	state   <=	RG;
+            	count   <=	X"0";
             
         elsif rising_edge(clk) then
         	
@@ -34,6 +34,7 @@ begin
                     	state <= RO;
                         count <= X"0";
                     end if;
+			    
                 when RO => 
                 	if count < SEC1 then
                     	state <= RO;
@@ -42,6 +43,7 @@ begin
                     	state <= RR1;
                         count <= X"0";
                     end if;
+			    
                 when RR1 => 
                 	if count < SEC1 then
                     	state <= RR1;
@@ -50,6 +52,7 @@ begin
                     	state <= GR;
                         count <= X"0";
                     end if;
+			    
                when GR => 
                 	if count < SEC5 then
                     	state <= GR;
@@ -58,6 +61,7 @@ begin
                     	state <= OR1;
                         count <= X"0";
                     end if;
+			    
                when OR1 => 
                 	if count < SEC1 then
                     	state <= OR1;
@@ -66,6 +70,7 @@ begin
                     	state <= RR2;
                         count <= X"0";
                     end if;
+			    
                when RR2 => 
                 	if count < SEC1 then
                     	state <= RR2;
@@ -74,21 +79,22 @@ begin
                     	state <= RG;
                         count <= X"0";
                     end if;
+			    
        			when others => state <= RG;
              end case;
          end if;
     end process;
     
-    C2: process(state)
+    C : process(state)
     	begin
-   			case state is 
+   	    case state is 
             	when RG 	=> light <= "100001";
                 when RO 	=> light <= "100010";
-                when RR1    => light <= "100100";
+                when RR1   	=> light <= "100100";
                 when GR 	=> light <= "001100";
                 when OR1 	=> light <= "010100";
-                when RR2    => light <= "100100";
-                when others => light <= "100001";
+                when RR2        => light <= "100100";
+                when others     => light <= "100001";
             end case;
     end process;
     
