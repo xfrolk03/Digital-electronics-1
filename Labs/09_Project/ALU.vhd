9ced9b36886be6c10668478c87325e4950e65482
xfrolk03 <60733738+xfrolk03@users.noreply.github.com>
2020-04-30 22:23:17 +0200

    Update ALU.vhd

--- Labs/09_Project/ALU.vhd
+++ Labs/09_Project/ALU.vhd
@@ -5,7 +5,7 @@
 -- Create Date:    06:04:57 04/20/2020 
 -- Design Name: 
 -- Module Name:    ALU - Behavioral 
--- Project Name:   ALU
+-- Project Name: 	 ALU
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -17,12 +17,12 @@ entity ALU is
 	 
 	 A     		: in   STD_LOGIC_VECTOR(3 downto 0);  
 	 B     		: in   STD_LOGIC_VECTOR(3 downto 0);
-    	 OP   		: in   STD_LOGIC_VECTOR(3 downto 0);
+    OP   		: in   STD_LOGIC_VECTOR(3 downto 0);
 	 clk_i 		: in 	 STD_LOGIC;
 	 srst_n_i	: in 	 STD_LOGIC;
 	 
-    	 ALU_Out	: out  STD_LOGIC_VECTOR(3 downto 0); --vrací výsledek
-	 underflow_o	: out  STD_LOGIC;
+    ALU_Out	 	: out  STD_LOGIC_VECTOR(7 downto 0); --vrací výsledek
+	 underflow_o: out  STD_LOGIC;
 	 carry_o 	: out  STD_LOGIC
 	 );
 	 
@@ -30,8 +30,8 @@ end ALU;
 
 architecture Behavioral of ALU is
 
-	signal inp_A, inp_B, RES 	: unsigned(3 downto 0) := (others => '0');
-
+	signal inp_A, inp_B, RES : unsigned(3 downto 0) := (others => '0');
+	signal MULT_RES : unsigned(7 downto 0) := (others => '0');
 begin
 
 	inp_A <= unsigned(A);
@@ -40,88 +40,103 @@ begin
    process(clk_i)
 	begin
 		if (rising_edge(clk_i)) then
-			carry_o 	<= '0';
-			underflow_o 	<= '0';
-
-			case(OP)is	  
-				when "0000" => 
-					if((inp_A + inp_B) > "1111") then
-						carry_o <= '1';
-					end if;
-					
-					RES <= (inp_A + inp_B); 
+			carry_o 		<= '0';
+			underflow_o <= '0';
+			MULT_RES <= (others => '0');
+			RES <= (others => '0');
 			
-				when "0001" => 
-					if((inp_A - inp_B) < "0000") then
-						underflow_o <= '0';         --signal podteceni
-					end if;
-					
-					RES <= inp_A - inp_B;
-			
-				when "0010" => 
-					if((inp_A + 1) > "1111") then
-						carry_o <= '1';
-					end if;
-					
-					RES <= inp_A + 1;
-								
-				when "0011" => 
-					if((inp_A - 1) < "0000") then
-						underflow_o <= '0';	   --signal podteceni
-					end if;
-					
-					RES <= inp_A - 1;
-			
-				when "0100" => 
-					if((inp_B + 1) > "1111") then
-						carry_o <= '1';
-					end if;
-					
-					RES <= inp_B + 1;							
-			
-				when "0101" => 
-					RES <= inp_A * inp_B;
-					if((inp_A * inp_B) > "1111") then
-						carry_o <= '1';
-					end if;				   --zobrazeni ve dvou cifrach??
-					
-					RES <= inp_A * inp_B;
-					
-				when "0110" => RES <= NOT inp_A ;
-			
-				when "0111" => RES <= NOT inp_B;
-			
-				when "1000" => RES <= inp_A AND inp_B;
-			
-				when "1001" => RES <= inp_A OR inp_B;
-			
-				when "1010" => RES <= inp_A XOR inp_B;
-			
-				when "1011" => RES <= inp_A NAND inp_B;
-			
-				when "1100" => RES <= inp_A NOR inp_B;
-			
-				when "1101" => RES <= inp_A XNOR inp_B;
-			
-				when "1110" => 
-					if(inp_A > inp_B) then
-					RES <= x"0" ;
-					else
-					RES <= x"1" ;
-					end if;
-			
-				when "1111" => 
-					if(inp_A = inp_B) then
-						RES <= x"1" ;
-					else
-						RES<= x"0" ;
-					end if;				
-				when others => RES <= "0000" ;
+			if(srst_n_i = '0') then			--synchronni reset
+				carry_o 		<= '0';
+				underflow_o <= '0';
+				MULT_RES <= (others => '0');
+				RES <= (others => '0');
+				ALU_Out <=(others => '0');
+			else	
+				case(OP)is	  
+					when "0000" => 
+						if((inp_A + inp_B) > "1111") then
+							carry_o <= '1';
+						end if;
+						
+						RES <= (inp_A + inp_B); 
 				
-			end case;
+					when "0001" => 
+						if((inp_A - inp_B) < "0000") then
+							underflow_o <= '1';              --signal podteceni
+						end if;
+						
+						RES <= inp_A - inp_B;
+				
+					when "0010" => 
+						if((inp_A + 1) > "1111") then
+							carry_o <= '1';
+						end if;
+						
+						RES <= inp_A + 1;
+									
+					when "0011" => 
+						if((inp_A - 1) < "0000") then
+							underflow_o <= '1';		--signal podteceni
+						end if;
+						
+						RES <= inp_A - 1;
+				
+					when "0100" => 
+						if((inp_B + 1) > "1111") then
+							carry_o <= '1';
+						end if;
+						
+						RES <= inp_B + 1;							
+				
+					when "0101" => 
+					
+						if((inp_A * inp_B) > "1111") then
+							carry_o <= '1';
+						end if;										--zobrazeni ve dvou cifrach??
+						
+						MULT_RES <= inp_A * inp_B;
+						
+					when "0110" => RES <= NOT inp_A ;
+				
+					when "0111" => RES <= NOT inp_B;
+				
+					when "1000" => RES <= inp_A AND inp_B;
+				
+					when "1001" => RES <= inp_A OR inp_B;
+				
+					when "1010" => RES <= inp_A XOR inp_B;
+				
+					when "1011" => RES <= inp_A NAND inp_B;
+				
+					when "1100" => RES <= inp_A NOR inp_B;
+				
+					when "1101" => RES <= inp_A XNOR inp_B;
+				
+					when "1110" => 
+						if(inp_A > inp_B) then
+						RES <= x"0" ;
+						else
+						RES <= x"1" ;
+						end if;
+				
+					when "1111" => 
+						if(inp_A = inp_B) then
+							RES <= x"1" ;
+						else
+							RES<= x"0" ;
+						end if;				
+					when others => RES <= "0000" ;
+				
+				end case;			
+			end if;
+			
+			if(op = "0101") then
+				ALU_Out <= std_logic_vector(MULT_RES);
+			else
+				ALU_Out <= "0000" & std_logic_vector(RES); 
+			end if;
+			
 		end if;
-	end process;
- 
-	ALU_Out <= std_logic_vector(RES); 
+	end process;	
  
 end architecture;
